53dbad9cf6a41fc8f966c8903749ada64db7f58b
unknown <olawanletemitayo@gmail.com>
2022-10-04 05:48:23 +0100

    solved twwo-sum from leetcode

---
+++ two_sum.ts
@@ -0,0 +1,23 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const twoSum = (nums: number[], target: number): number[] => {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+};
